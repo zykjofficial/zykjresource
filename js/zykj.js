e5e3b4fc795be254cb76ec53b9b644e5475d4571
zykjofficial <1454013857@qq.com>
2020-06-28 11:15:15 +0800

    resource V0.5

--- js/zykj.js
+++ js/zykj.js
@@ -96,12 +96,15 @@ if (returnCitySN) {
 
 //手机侧边栏默认不展开
 var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " menus-closed";
+if(mobile_sidebar_menus != null){
+	var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
+	var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+    for (var i = 0; i < menus_item_child.length; i++) {
+        menus_item_child[i].style.display = "none";
+        menus_expand[i].className += " menus-closed";
+    }
 }
+
 
 //随机图片 
 var full_page = document.getElementsByClassName("full_page")[0];
